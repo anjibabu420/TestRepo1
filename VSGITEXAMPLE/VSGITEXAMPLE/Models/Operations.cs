9a11bfb048c02bb522f5b6c6a3553b9cf2d34569
anjibabu420 <anji.vtalent@gmail.com>
2019-06-26 16:13:16 +0530

    Home Controller Added and Operation class Created under model folder

---
+++ VSGITEXAMPLE/VSGITEXAMPLE/Models/Operations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSGITEXAMPLE.Models
+{
+    public class Operations
+    {
+        public int Addition()
+        {
+            return 0;
+        }
+    }
+}
